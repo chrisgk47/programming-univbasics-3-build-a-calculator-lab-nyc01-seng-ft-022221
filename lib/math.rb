d48242686eaf28d002a02f900c5cdf9b8b8642c2
Christopher Kim <chris.kim1990@gmail.com>
2021-02-05 01:54:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,31 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  subtract = "#{num1} - #{num2}"
+  subtract = num1 - num2
   puts subtract
   subtract
 end
 
 def division(num1, num2)
-  divide = "#{num1} / #{num2}"
+  divide = num1 / num2
   puts divide
   divide
 end
 
 def multiplication(num1, num2)
-  multiply = "#{num1} * #{num2}"
+  multiply = num1 * num2
   puts multiply
   multiply
 end
 
 def modulo(num1, num2)
-  remainder = "#({num1} % #{num2})"
+  remainder = num1 % num2
   puts remainder
   remainder
 end
 
 def square_root(num)
-  square = "num.sqrt"
+  square = num.sqrt
   puts square
   square
 end
